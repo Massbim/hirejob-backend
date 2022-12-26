e45c4491b422cc5ad6e8e2232157b8dce5426344
Alamsyah Bimasakti <alamsyahbima22@gmail.com>
2022-12-26 18:17:31 +0700

    fix cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://hirejob-snowy.vercel.app",
   })
 );
 app.use(morgan("dev"));
